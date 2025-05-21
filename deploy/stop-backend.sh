94d6fceb93f2bb98e03973dc6585a5662f154e85
HamzaDamouh <damouhamza11@gmail.com>
2025-05-21 16:26:52 +0200

    final commit

--- deploy/stop-backend.sh
+++ deploy/stop-backend.sh
@@ -6,7 +6,6 @@ PORT=45065
 
 echo "Stopping backend on $VM_HOST…"
 
-# -T disables pseudo-tty allocation so we don't get the motd banner
 ssh -T $VM_USER@$VM_HOST <<EOF
   # look up the PID listening on our TCP port
   PID=\$(lsof -ti tcp:$PORT)
